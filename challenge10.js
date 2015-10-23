ce12fcfa43c3688abdb8aef008cd6abdbf93b968
Jacob Cole <tmad4000@gmail.com>
2015-10-23 15:10:56 -0700

    moar

--- challenge10.js
+++ challenge10.js
@@ -1,10 +1,7 @@
 
 var strftime = require('strftime') 
- 
-
-
-
 var net = require('net')
+
 var server = net.createServer(function (socket) {
   // socket handling logic
   socket.write(strftime('%F %H:%M'))
